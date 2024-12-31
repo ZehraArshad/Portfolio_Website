a20c224feabd6d6a8d25ba15de6e796d3b6c005e
Zehra <zehraarshadmulla@gmail.com>
2025-01-01 02:31:15 +0500

    Navbar done

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,22 +1,11 @@
-
+import logo from "./assets/img/logo.svg"
 import './App.css';
-
+import { NavBar } from "./components/Navbar";
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar/>
     </div>
   );
 }
