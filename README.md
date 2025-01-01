db27d3ca1587dee20d49bcef08e9ce068764ac84
Zehra <zehraarshadmulla@gmail.com>
2025-01-02 01:47:53 +0500

    customization

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ git add .
 git commit -m "some message" 
 git push
 following this video
-https://www.youtube.com/watch?v=hYv6BM2fWd8&list=PLd0qRSEDFQI7DgB5BKHQrryFlR02n1bK-
+https://www.youtube.com/watch?v=hYv6BM2fWd8&list=PLd0qRSEDFQI7DgB5BKHQrryFlR02n1bK- at 39:20
--- portfolio/src/components/Banner.js
+++ portfolio/src/components/Banner.js
@@ -11,7 +11,7 @@ export const Banner = () => {
   const [text, setText] = useState('');
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const [index, setIndex] = useState(1);
-  const toRotate = [ "Web Developer", "Web Designer", "UI/UX Designer" ];
+  const toRotate = [ "Data Analyst", "Speaker", "Data Scientist","AI Enthusiast" ];
   const period = 2000;
 
   useEffect(() => {
@@ -56,8 +56,11 @@ export const Banner = () => {
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline">Welcome to my Portfolio</span>
-                <h1>{`Hi! I'm Judy`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
-                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <h1>{`Hi! I'm Zehra, A`} <span className="txt-rotate" dataPeriod="1000" data-rotate={toRotate}><span className="wrap">{text}</span></span></h1>
+                  <p>Data is more than numbers; it's a story waiting to be told. Hidden within every dataset are patterns that can 
+                    shape decisions, inspire innovation, and transform the world. My journey as a data scientist is about uncovering 
+                    those stories—digging deep into the raw, unfiltered noise to extract insights that matter. It's about connecting the 
+                    dots, finding the unseen, and giving voice to the silent truths buried in data.</p>
                   <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
               </div>}
             </TrackVisibility>
